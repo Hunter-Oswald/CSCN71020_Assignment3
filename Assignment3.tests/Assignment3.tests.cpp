80f65fd6d3e8c1a3b967d4486304589b122b48e0
Hunter Oswald <Hoswald1756@conestogac.on.ca>
2021-11-07 15:58:00 -0500

    Add project files.

---
+++ Assignment3.tests/Assignment3.tests.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace Assignment3tests
+{
+	TEST_CLASS(Assignment3tests)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
---
+++ CSCN71020_Assignment3/Source.c
@@ -0,0 +1,10 @@
+// soft impl - assignment 3 - rock paper scissors
+// Hunter Oswald
+
+#include <stdio.h>
+
+int main(void) {
+
+
+	return (0);
+}
